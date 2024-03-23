7da6581107aff5317a558ef3294b5131f8a1dbd2
Elhamirzaheidar <emirzaheidar@gmail.com>
2024-03-23 04:00:58 -0700

    SnappProject

---
+++ data/data.js
@@ -0,0 +1,430 @@
+import image1 from "../src/assets/Img/Moving.png";
+import image2 from "../src/assets/Img/Pickup-new.png";
+import image3 from "../src/assets/Img/bus.png";
+import image4 from "../src/assets/Img/fdom-t.png";
+import image5 from "../src/assets/Img/hotel.png";
+import image6 from "../src/assets/Img/Pickup-new.png";
+import image7 from "../src/assets/Img/bus.png";
+import image8 from "../src/assets/Img/fdom-t.png";
+import image1_1 from "../src/assets/Img/image1.png";
+import image1_2 from "../src/assets/Img/image2.png";
+import image1_3 from "../src/assets/Img/image3.png";
+import image1_4 from "../src/assets/Img/image4.png";
+import image2_1 from "../src/assets/Img/Food.jpg";
+import image2_2 from "../src/assets/Img/Driver.jpg";
+import image2_3 from "../src/assets/Img/Club.jpg";
+import image2_4 from "../src/assets/Img/Market.jpg";
+import image3_1 from "../src/assets/Img/210x190-easy.png";
+import image3_2 from "../src/assets/Img/210x190-eco.png";
+import image3_3 from "../src/assets/Img/210x190-fast.png";
+import image4_1 from "../src/assets/Img/180x100-income.png";
+import image4_2 from "../src/assets/Img/180x100-hour.png";
+import image4_3 from "../src/assets/Img/180x100-benefits.png";
+import image4_4 from "../src/assets/Img/180x100-carfix.png";
+import Logo1 from "../src/assets/Img/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHZpZXdCb3g9IjAgMCAzMiAzMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48ZyBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiPjxnIHN0cm9rZT0iIzMyMzIzMiIgc3Ryb2tlLWx.svg";
+import Logo2 from "../src/assets/Img/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHZpZXdCb3g9IjAgMCAzMiAzMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48ZyBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiPjxwYXRoIHN0cm9rZT0iIzMyMzIzMiIgc3R (1).svg";
+import Logo3 from "../src/assets/Img/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHZpZXdCb3g9IjAgMCAzMiAzMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48ZyBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiPjxwYXRoIHN0cm9rZT0iIzMyMzIzMiIgc3R (2).svg";
+import Logo4 from "../src/assets/Img/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHZpZXdCb3g9IjAgMCAzMiAzMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48ZyBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiPjxwYXRoIHN0cm9rZT0iIzMyMzIzMiIgc3Ryb2t.svg";
+import Namad1 from "../src/assets/Img/logo.png";
+import Namad2 from "../src/assets/Img/namad.png";
+import Carousel1 from "../src/assets/Img/Food.jpg"
+import Carousel2 from "../src/assets/Img/Driver.jpg"
+import Carousel3 from "../src/assets/Img/Club.jpg"
+import Carousel4 from "../src/assets/Img/Market.jpg"
+export const InformationNavbar = [
+  {
+    id: 1,
+    text: "فرصت های شغلی",
+    url: "https://career.snapp.ir/?utm_source=snapp.ir&utm_medium=website-button&utm_campaign=menu",
+  },
+  {
+    id: 2,
+    text: "باشگاه رانندگان",
+    url: "https://club.snapp.ir/?utm_source=snapp.ir&utm_medium=website-button&utm_campaign=menu",
+  },
+  { id: 3, text: "بلاگ", url: "https://snapp.ir/blog/" },
+  {
+    id: 4,
+    text: "ثبت نام راننده اسنپ",
+    url: "https://digitalsignup.snapp.ir/?utm_source=snapp.ir&utm_medium=website-button&utm_campaign=menu",
+  },
+  { id: 5, text: " پنل سازمانی", url: "https://snapp.ir/blog/" },
+  { id: 6, text: " درباره ما", url: "https://snapp.ir/about/" },
+  { id: 7, text: "تماس با ما", url: "https://snapp.ir/contact/" },
+];
+export const InformationFooter = [
+  {
+    id: 1,
+    text: "فرصت های شغلی",
+    url: "https://career.snapp.ir/?utm_source=snapp.ir&utm_medium=website-button&utm_campaign=menu",
+  },
+
+  { id: 2, text: "بلاگ", url: "https://snapp.ir/blog/" },
+  { id: 3, text: " شرایط و قوانین", url: "https://snapp.ir/terms/ " },
+  {
+    id: 4,
+    text: " پنل سازمانی",
+    url: "https://corporate.snapp.taxi/auth/login",
+  },
+  { id: 5, text: "سوالات متداول", url: "https://snapp.ir/faq/" },
+  {
+    id: 6,
+    text: "باشگاه رانندگان",
+    url: "https://club.snapp.ir/?utm_source=snapp.ir&utm_medium=website-button&utm_campaign=menu",
+  },
+  {
+    id: 7,
+    text: "ثبت نام راننده اسنپ",
+    url: "https://digitalsignup.snapp.ir/?utm_source=snapp.ir&utm_medium=website-button&utm_campaign=menu",
+  },
+  { id: 8, text: " درباره ما", url: "https://snapp.ir/about/" },
+  { id: 9, text: "تماس با ما", url: "https://snapp.ir/contact/" },
+];
+export const imagesListSecond = [
+  {
+    id: 1,
+    src: image1,
+    alt: "Image 1",
+    url: "https://redirect.appmetrica.yandex.com/serve/100056237185439217",
+    title: " اسباب کشی منزل",
+    Describe: "حمل و نقل بااسنپ",
+  },
+  {
+    id: 2,
+    src: image2,
+    alt: "Image 2",
+    url: "https://cafebazaar.ir/app/cab.snapp.passenger?referrer=appmetrica_tracking_id=243413381082522868&ym_tracking_id=5095385553448618405",
+    title: "درخواست وانت",
+    Describe: "حمل و نقل بااسنپ",
+  },
+  {
+    id: 3,
+    src: image3,
+    alt: "Image 3",
+    url: "https://snapp.ir/bus-ticket/",
+    title: " بلیط اتوبوس",
+    Describe: "گردشگری بااسنپ",
+  },
+  {
+    id: 4,
+    src: image4,
+    alt: "Image 4",
+    url: "https://app.snapp.taxi/login/?redirect_to=%2Fpre-ride%3Futm_source%3Dwebsite%2Cutm_medium%3Dwebapp-button",
+    title: "بلیط پروازداخلی",
+    Describe: "گردشگری بااسنپ",
+  },
+  {
+    id: 5,
+    src: image5,
+    alt: "Image 5",
+    url: "https://app.snapp.taxi/login/?redirect_to=%2Fpre-ride%3Futm_source%3Dwebsite%2Cutm_medium%3Dwebapp-button",
+    title: "رزروهتل",
+    Describe: "گردشگری بااسنپ",
+  },
+  {
+    id: 6,
+    src: image6,
+    alt: "Image 6",
+    url: "https://app.snapp.taxi/login/?redirect_to=%2Fpre-ride%3Futm_source%3Dwebsite%2Cutm_medium%3Dwebapp-button",
+    title: "درخواست وانت",
+    Describe: "حمل و نقل بااسنپ",
+  },
+  {
+    id: 7,
+    src: image7,
+    alt: "Image 7",
+    url: "https://snapp.ir/bus-ticket/",
+    title: " بلیط اتوبوس",
+    Describe: "گردشگری بااسنپ",
+  },
+  {
+    id: 8,
+    src: image8,
+    alt: "Image 8",
+    url: "https://app.snapp.taxi/login/?redirect_to=%2Fpre-ride%3Futm_source%3Dwebsite%2Cutm_medium%3Dwebapp-button",
+    title: "بلیط پروازداخلی",
+    Describe: "گردشگری بااسنپ",
+  },
+  {
+    id: 9,
+    src: image1,
+    alt: "Image 1",
+    url: "https://redirect.appmetrica.yandex.com/serve/100056237185439217",
+    title: " اسباب کشی منزل",
+    Describe: "حمل و نقل بااسنپ",
+  },
+  {
+    id: 10,
+    src: image2,
+    alt: "Image 2",
+    url: "https://cafebazaar.ir/app/cab.snapp.passenger?referrer=appmetrica_tracking_id=243413381082522868&ym_tracking_id=5095385553448618405",
+    title: "درخواست وانت",
+    Describe: "حمل و نقل بااسنپ",
+  },
+  {
+    id: 11,
+    src: image3,
+    alt: "Image 3",
+    url: "https://snapp.ir/bus-ticket/",
+    title: " بلیط اتوبوس",
+    Describe: "گردشگری بااسنپ",
+  },
+  {
+    id: 12,
+    src: image4,
+    alt: "Image 4",
+    url: "https://app.snapp.taxi/login/?redirect_to=%2Fpre-ride%3Futm_source%3Dwebsite%2Cutm_medium%3Dwebapp-button",
+    title: "بلیط پروازداخلی",
+    Describe: "گردشگری بااسنپ",
+  },
+  {
+    id: 13,
+    src: image5,
+    alt: "Image 5",
+    url: "https://app.snapp.taxi/login/?redirect_to=%2Fpre-ride%3Futm_source%3Dwebsite%2Cutm_medium%3Dwebapp-button",
+    title: "رزروهتل",
+    Describe: "گردشگری بااسنپ",
+  },
+  {
+    id: 14,
+    src: image6,
+    alt: "Image 6",
+    url: "https://app.snapp.taxi/login/?redirect_to=%2Fpre-ride%3Futm_source%3Dwebsite%2Cutm_medium%3Dwebapp-button",
+    title: "درخواست وانت",
+    Describe: "حمل و نقل بااسنپ",
+  },
+  {
+    id: 15,
+    src: image7,
+    alt: "Image 7",
+    url: "https://app.snapp.taxi/login/?redirect_to=%2Fpre-ride%3Futm_source%3Dwebsite%2Cutm_medium%3Dwebapp-button",
+    title: "سرمایه گذاری",
+    Describe: "گردشگری بااسنپ",
+  },
+  {
+    id: 16,
+    src: image8,
+    alt: "Image 8",
+    url: "https://app.snapp.taxi/login/?redirect_to=%2Fpre-ride%3Futm_source%3Dwebsite%2Cutm_medium%3Dwebapp-button",
+    title: "بلیط پروازداخلی",
+    Describe: "گردشگری بااسنپ",
+  },
+  {
+    id: 17,
+    src: image1,
+    alt: "Image 1",
+    url: "https://redirect.appmetrica.yandex.com/serve/100056237185439217",
+    title: " اسباب کشی منزل",
+    Describe: "حمل و نقل بااسنپ",
+  },
+  {
+    id: 18,
+    src: image2,
+    alt: "Image 2",
+    url: "https://cafebazaar.ir/app/cab.snapp.passenger?referrer=appmetrica_tracking_id=243413381082522868&ym_tracking_id=5095385553448618405",
+    title: "درخواست وانت",
+    Describe: "حمل و نقل بااسنپ",
+  },
+  {
+    id: 19,
+    src: image3,
+    alt: "Image 3",
+    url: "https://snapp.ir/bus-ticket/",
+    title: " بلیط اتوبوس",
+    Describe: "گردشگری بااسنپ",
+  },
+  {
+    id: 20,
+    src: image4,
+    alt: "Image 4",
+    url: "https://app.snapp.taxi/login/?redirect_to=%2Fpre-ride%3Futm_source%3Dwebsite%2Cutm_medium%3Dwebapp-button",
+    title: "بلیط پروازداخلی",
+    Describe: "گردشگری بااسنپ",
+  },
+  {
+    id: 21,
+    src: image5,
+    alt: "Image 5",
+    url: "https://app.snapp.taxi/login/?redirect_to=%2Fpre-ride%3Futm_source%3Dwebsite%2Cutm_medium%3Dwebapp-button",
+    title: "رزروهتل",
+    Describe: "گردشگری بااسنپ",
+  },
+];
+
+export const imagesList = [
+  {
+    id: 1,
+    src: image1_1,
+    alt: "Image 1",
+    url: "https://redirect.appmetrica.yandex.com/serve/100056237185439217",
+  },
+  {
+    id: 2,
+    src: image1_2,
+    alt: "Image 2",
+    url: "https://cafebazaar.ir/app/cab.snapp.passenger?referrer=appmetrica_tracking_id=243413381082522868&ym_tracking_id=5095385553448618405",
+  },
+  {
+    id: 3,
+    src: image1_3,
+    alt: "Image 3",
+    url: "https://apps.apple.com/us/app/%D8%A7%D8%B3%D9%86%D9%BE-snapp/id996370173?referrer=appmetrica_tracking_id%3D1109100489301122257%26ym_tracking_id%3D4090188548324462884",
+  },
+  {
+    id: 4,
+    src: image1_4,
+    alt: "Image 4",
+    url: "https://app.snapp.taxi/login/?redirect_to=%2Fpre-ride%3Futm_source%3Dwebsite%2Cutm_medium%3Dwebapp-button",
+  },
+];
+export const slides = [
+  {
+    id: 1,
+    src: image2_1,
+    alt: "Image 1",
+    url: "https://redirect.appmetrica.yandex.com/serve/100056237185439217",
+  },
+  {
+    id: 2,
+    src: image2_2,
+    alt: "Image 2",
+    url: "https://cafebazaar.ir/app/cab.snapp.passenger?referrer=appmetrica_tracking_id=243413381082522868&ym_tracking_id=5095385553448618405",
+  },
+  {
+    id: 3,
+    src: image2_3,
+    alt: "Image 3",
+    url: "https://apps.apple.com/us/app/%D8%A7%D8%B3%D9%86%D9%BE-snapp/id996370173?referrer=appmetrica_tracking_id%3D1109100489301122257%26ym_tracking_id%3D4090188548324462884",
+  },
+  {
+    id: 4,
+    src: image2_4,
+    alt: "Image 4",
+    url: "https://app.snapp.taxi/login/?redirect_to=%2Fpre-ride%3Futm_source%3Dwebsite%2Cutm_medium%3Dwebapp-button",
+  },
+];
+export const BoxList = [
+  {
+    id: 1,
+    src: image3_1,
+    alt: "image3_1",
+    Title: "آسان",
+    Describe:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحاه روزنامه و مجله در ستون و سطرآنچنان که لازم است ",
+  },
+  {
+    id: 2,
+    src: image3_3,
+    alt: "image3_3",
+    Title: "سریع",
+    Describe:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحاننامه و مجله در ستون و سطرآنچنان که لازم است ",
+  },
+  {
+    id: 3,
+    src: image3_2,
+    alt: "image3_2",
+    Title: "به صرفه",
+    Describe:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده انامه و مجله در ستون و سطرآنچنان که لازم است ",
+  },
+];
+export const ThirdSectionList = [
+  {
+    id: 1,
+    src: image4_1,
+    alt: "image4_1",
+    Title: "درآمدتضمینی+پاداش های ماهانه و هفتگی",
+    Describe:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده انامه و مجله در ستون و سطرآنچنان که لازم است ",
+  },
+  {
+    id: 2,
+    src: image4_2,
+    alt: "image4_2",
+    Title: " ساعت کاری دلخواه",
+    Describe:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده انامه و مجله در ستون و سطرآنچنان که لازم است ",
+  },
+  {
+    id: 3,
+    src: image4_3,
+    alt: "image4_3",
+    Title: " مزایا و خدمات باشگاه رانندگان",
+    Describe:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده انامه و مجله در ستون و سطرآنچنان که لازم است ",
+  },
+  {
+    id: 4,
+    src: image4_4,
+    alt: "image4_4",
+    Title: "کارفیکس",
+    Describe:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده انامه و مجله در ستون و سطرآنچنان که لازم است ",
+  },
+];
+export const ImageLogo = [
+  {
+    id: 1,
+    src: Logo1,
+    alt: "Logo1",
+    url: "https://twitter.com/snapp_team",
+  },
+  {
+    id: 2,
+    src: Logo2,
+    alt: "Logo2",
+    url: " https://twitter.com/snapp_team",
+  },
+  {
+    id: 3,
+    src: Logo3,
+    alt: "Logo3",
+    url: "https://instagram.com/snapp_team",
+  },
+  {
+    id: 4,
+    src: Logo4,
+    alt: "Logo4",
+    url: "https://twitter.com/snapp_team",
+  },
+];
+export const ImageNamad = [
+  {
+    id: 1,
+    src: Namad1,
+    alt: "Namad1",
+    url: " https://twitter.com/snapp_team",
+  },
+  {
+    id: 2,
+    src: Namad2,
+    alt: "Namad2",
+    url: "https://twitter.com/snapp_team",
+  },
+];
+export const CarouselImg=[{
+    
+    id: 1,
+    src: {Carousel1},
+    alt: "Carousel1",
+    url: " https://twitter.com/snapp_team",
+  },
+  {
+    id: 2,
+    src: {Carousel2},
+    alt: "Carousel2",
+    url: "https://twitter.com/snapp_team",
+  },  {
+    id: 3,
+    src: {Carousel3},
+    alt: "Carousel3",
+    url: " https://twitter.com/snapp_team",
+  },
+  {
+    id: 4,
+    src: {Carousel4},
+    alt: "Carousel4",
+    url: "https://twitter.com/snapp_team",
+  },
+]
